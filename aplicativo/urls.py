357bcf4c1a63c80577a6b837eb24361839aab3de
Matheus Lemes <mathtass@hotmail.com>
2022-11-15 20:18:51 -0300

    tres telas prontas

--- aplicativo/urls.py
+++ aplicativo/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('recarga', views.recarga, name='recarga'),
+    path('comprar_bilhete', views.comprar_bilhete, name='comprar_bilhete'),
+    path('usar_bilhete', views.usar_bilhete, name='usar_bilhete'),
 ]
--- aplicativo/views.py
+++ aplicativo/views.py
@@ -21,7 +21,30 @@ def recarga(request):
         nome = request.POST['name']
         recarga = request.POST['recarga']
         busca = Banco.objects.filter(nome=nome)
-        saldo_atual = int(recarga) + busca[0].numero
+        saldo_atual = int(recarga) + busca[0].saldo
         busca[0].saldo = saldo_atual
         Banco.objects.filter(nome=nome).update(saldo=saldo_atual)
     return render(request, 'aplicativo/recarga.html')
+
+
+def comprar_bilhete(request):
+    nome = ""
+    banco = Banco.objects.all().order_by('nome')
+    compra =  ''
+    if request.method=='POST':
+        nome = request.POST['name']
+        bilhetes = request.POST['bilhetes']
+        banco = Banco.objects.filter(nome=nome)
+        preco = int(bilhetes) * int(3)
+        if (int(banco[0].saldo) > int(preco)):
+            numero_bilhetes = int(bilhetes) + banco[0].numero
+            saldo_atual = int(banco[0].saldo) - 3
+            Banco.objects.filter(nome=nome).update(numero=numero_bilhetes,saldo=saldo_atual)  
+            compra = 'true'
+        else:
+            compra = 'false'
+    return render(request, 'aplicativo/comprar_bilhete.html', {'banco': banco, 'nome':nome, 'compra':compra})
+
+
+def usar_bilhete(request):
+    pass
